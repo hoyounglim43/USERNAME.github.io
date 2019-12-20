6b7c3f68a6339340ee221a0d7b0dd3b2f0a9e228
Hoyoung Lim <hoyounglim43@gmail.com>
2019-12-20 11:15:13 +1100

    Migrating Code

---
+++ 19-tom-jerry-maths-quiz-subtraction/index.js
@@ -0,0 +1,53 @@
+var scoreHTML = document.querySelector(".score")
+var firstCheesesHTML = document.querySelector(".first-cheeses")
+var secondCheesesHTML = document.querySelector(".second-cheeses")
+var answerHTML = document.querySelector(".answer")
+
+var score = 0
+var counter = 0
+var answer
+
+function generateRandomNumber() {
+    var number = Math.ceil(Math.random() * 10)
+    return number
+}
+
+function displayQuestion() {
+    answerHTML.focus()
+    var number1 = generateRandomNumber()
+    var number2 = generateRandomNumber()
+    answer = number1 + number2
+    for (var i = 0; i < number1; i++) {
+        var image = document.createElement("img")
+        image.src = "https://www.stickpng.com/assets/thumbs/580b57fbd9996e24bc43c0ca.png"
+        firstCheesesHTML.appendChild(image)
+    }
+    for (var i = 0; i < number2; i++) {
+        var image = document.createElement("img")
+        image.src = "https://www.stickpng.com/assets/thumbs/580b57fbd9996e24bc43c0ca.png"
+        secondCheesesHTML.appendChild(image)
+    }
+}
+
+displayQuestion()
+
+answerHTML.addEventListener('keyup', handleAnswerInput)
+
+function handleAnswerInput() {
+    if (event.key == "Enter") {
+        counter += 1
+        if (answer == answerHTML.value) {
+            score += 1
+        }
+        answerHTML.value = ""
+        scoreHTML.innerHTML = `Score: ${score}/${counter}`
+        while (firstCheesesHTML.firstChild) {
+            firstCheesesHTML.removeChild(firstCheesesHTML.firstChild)
+        }
+        while (secondCheesesHTML.firstChild) {
+            secondCheesesHTML.removeChild(secondCheesesHTML.firstChild)
+        }
+        displayQuestion()
+    }
+}
+
